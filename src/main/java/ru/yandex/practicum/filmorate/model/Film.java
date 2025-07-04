b729c720f9ce6afe3c75fc3f3cb917929f9142f7
Egorov Igor <igorokegorov@yandex.ru>
2025-07-04 11:36:17 +0300

    Реализация ТЗ 10

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,44 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
 public class Film {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private int duration;
+
+    private final static Logger log = LoggerFactory.getLogger(Film.class);
+
+    public void validate() {
+        if (getName() == null || getName().isBlank()) {
+            log.error("Валидация не пройдена: название фильма не может быть пустым, name={}", name);
+            throw new ValidationException("Название не может быть пустым");
+        }
+
+        if (getDescription().length() > 200) {
+            log.error("Валидация не пройдена: длина описания превышает 200 символов, description.length={}", description.length());
+            throw new ValidationException("Максимальная длина описания не может быть более 200 символов");
+        }
+
+        if (getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.error("Валидация не пройдена: дата релиза не может быть ранее 28 декабря 1895 года или null, releaseDate={}", releaseDate);
+            throw new ValidationException("Дата релиза не может быть ранее 28 декабря 1895 года");
+        }
+
+        if (getDuration() < 0) {
+            log.error("Валидация не пройдена: продолжительность фильма должна быть положительным числом, duration={}", duration);
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+        }
+
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+
+    private final static Logger log = LoggerFactory.getLogger(User.class);
+
+    public void validate() {
+        if (getEmail() == null || getEmail().isBlank() || !getEmail().contains("@")) {
+            log.error("Валидация не пройдена: электронная почта не может быть пустой и должна содержать символ @, email={}", email);
+            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
+        }
+
+        if (getLogin() == null || getLogin().isBlank() || getLogin().contains(" ")) {
+            log.error("Валидация не пройдена: логин не может быть пустым и содержать пробелы, login={}", login);
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы");
+        }
+
+        if (name == null || name.isBlank()) {
+            log.info("Имя пользователя пустое или null, используется логин: {}", login);
+            name = login;
+        }
+
+        if (getBirthday().isAfter(LocalDate.now())) {
+            log.error("Валидация не пройдена: дата рождения не может быть в будущем, birthday={}", birthday);
+            throw new ValidationException("Дата рождения не может быть в будущем.");
+        }
+    }
+}
