3cf796b2d4d2dfd352f5749da006453648c5f680
Egorov Igor <igorokegorov@yandex.ru>
2025-08-13 06:32:54 +0300

    Реализация ТЗ 12 (с исправлением)

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -7,7 +7,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 @Profile("memory")
 @Component
